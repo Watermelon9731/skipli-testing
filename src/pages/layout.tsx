4cd99d418fdc4891d5bb58289532c1ccdcf4152a
watermelon <tdchithinh@gmail.com>
2024-07-11 01:54:33 +0700

    Setup global font

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Home from "./home";
+import styled from "@emotion/styled";
+
+const StyledWrapper = styled.div`
+  font-family: "Monsterrat", sans-serif;
+`;
 
 export default function Layout() {
   return (
-    <div>
+    <StyledWrapper>
       <Home />
-    </div>
+    </StyledWrapper>
   );
 }
